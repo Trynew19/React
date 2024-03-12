a0429746a4066588d531c00485407c252cf7e43d
Trynew19 <chauhankrishana42@gmail.com>
2024-03-12 22:34:32 +0530

    gitbash

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: './.env' });
+const express = require('express')
+
+const app = express()
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`the server running on port :${process.env.PORT}`);
+})
+
+
+
+
+
+
+
+
+
+
+app
